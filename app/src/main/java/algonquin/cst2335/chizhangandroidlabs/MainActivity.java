a4f6d331fcec8b6c4b262408956dd4444349b9ad
chi zhang <zhan0627@algonquinlive.com>
2021-06-02 02:36:49 -0400

    finished phone version of lab 3

--- app/src/main/java/algonquin/cst2335/chizhangandroidlabs/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/chizhangandroidlabs/MainActivity.java
@@ -1,6 +1,11 @@
 package algonquin.cst2335.chizhangandroidlabs;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageview= findViewById(R.id.imageView);
+
+        Switch spinSwitch= findViewById(R.id.switch1);
+        spinSwitch.setOnCheckedChangeListener( (btn, isChecked) -> {
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+
+                imageview.startAnimation(rotate);
+            }
+            else {
+                imageview.clearAnimation();
+            }
+        });
     }
 }
